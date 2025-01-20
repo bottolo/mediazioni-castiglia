a3a5964e19daf46f633475b1dfa082f45afdbf00
bottolo <samuele.castiglia.98@gmail.com>
2025-01-20 17:48:24 +0100

    refactor: Changed routes structure

--- frontend/src/routes/about/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/about/')({
-  component: Index,
-})
-
-function Index() {
-  return <h1>About Page</h1>
-}
